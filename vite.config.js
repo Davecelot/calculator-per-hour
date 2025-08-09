f2eb721c54ed72f4590beb160dbce3136e8dd12b
davecelot <villarroeldiego708@gmail.com>
2025-08-08 21:41:55 -0300

    chore: use ESM config for Vite

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Este prefijo debe coincidir con el nombre del repositorio
-  base: '/calculator-per-hour/'
-});
